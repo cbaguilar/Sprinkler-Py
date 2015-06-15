710d83ce7e763c306325c8d709f5858db722557f
Mr. Frog <christian20002@users.noreply.github.com>
2015-06-14 19:14:06 -0700

    Update server.py

--- server.py
+++ server.py
@@ -191,6 +191,7 @@ def server_thread():
     while 1:
   	  conn, addr = s.accept()
  	  print 'Connected by', addr
+ 	  broad('Connected by'+str(addr))
 	  conn.send(program+'\n')
 	  while 1:
 	         data = conn.recv(4096)
